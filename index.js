abd605db1c45ec65525761fef2c93d8602e0b30a
Josiah Furrer <jbfjosiah@gmail.com>
2017-10-01 07:50:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 var name = "Janet"
 var height = 74
-console.log(parseInt(height) + 1 )
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+console.log(parseInt(height) + 1 )
